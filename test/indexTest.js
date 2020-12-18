7530a1cecbc877a51539bbe52182db4156419195
Thomas Lagies <thomas.lagies@sealsystems.de>
2020-12-18 09:39:41 +0100

    Added test objects

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,6 +5,118 @@ const assert = require('assertthat');
 const index = require('../lib');
 
 suite('index', () => {
+  const job = {
+    _id: '48727e72-20e8-446b-b0fd-9a00f212a07c',
+    orig: {
+      sourceHost: 'archlinux',
+      userName: 'tol',
+      jobName: '5Bgb',
+      fileName: '/git/cucumber-plossys/config/docs/document-a4.pdf',
+      printerName: 'printer1',
+      checkinType: 'lpr',
+      server: '99966e148466',
+      creator: '',
+      arrivedTime: 1608280547291.0,
+      checkinTime: 1608280547459.0,
+      fileSize: 1062429,
+      copies: 1,
+    },
+    current: {
+      sourceHost: 'archlinux',
+      userName: 'tol',
+      jobName: '5Bgb',
+      fileName: '/git/cucumber-plossys/config/docs/document-a4.pdf',
+      printerName: 'printer1',
+      checkinType: 'lpr',
+      server: '99966e148466',
+      creator: '',
+      arrivedTime: 1608280547291.0,
+      checkinTime: 1608280547459.0,
+      fileSize: 1062429,
+      copies: 1,
+      fileFormat: 'application/postscript',
+    },
+    status: 'job-waitprocessing',
+    fileName: ['799e3cb6-e58e-4048-a882-5b0c7a0b869f'],
+    stages: [
+      {
+        fileName: '48727e72-20e8-446b-b0fd-9a00f212a07c',
+        mimeType: 'application/pdf',
+        createdBy: 'checkin',
+        createdAt: 1608280547460.0,
+        context: {
+          type: 'lpr',
+        },
+      },
+      {
+        fileName: '799e3cb6-e58e-4048-a882-5b0c7a0b869f',
+        mimeType: 'application/postscript',
+        createdBy: 'convert-pdf2ps',
+        createdAt: 1608280548491.0,
+        context: {},
+      },
+    ],
+    refId: '48727e72',
+    lcCurrent: {
+      sourceHost: 'archlinux',
+      userName: 'tol',
+      jobName: '5bgb',
+      fileName: '/git/cucumber-plossys/config/docs/document-a4.pdf',
+      printerName: 'printer1',
+      checkinType: 'lpr',
+      server: '99966e148466',
+      creator: '',
+      arrivedTime: 1608280547291.0,
+      checkinTime: 1608280547459.0,
+      fileSize: 1062429,
+      copies: 1,
+      fileFormat: 'application/pdf',
+    },
+  };
+
+  const printer = {
+    _id: 'printer1',
+    config: {
+      printer: 'printer1',
+      connection: 'socket://printers:9100',
+      server: 'spooler1',
+      webUrl: 'http://printers',
+      type: {
+        name: 'printer1',
+        manufacturer: 'Netcat',
+        model: 'Henry',
+        pdl: {
+          name: 'postscript',
+          version: '3',
+        },
+      },
+      connectionHostname: 'printers',
+    },
+    runtime: {
+      status: 'printer-idle',
+      currentPrintingJob: '',
+      lastPrintedJobTime: 0,
+      queuedJobs: 0,
+    },
+  };
+
+  const metadata = {
+    status: 'status from metadata',
+    random: 12,
+    'random float': 51.138,
+    bool: false,
+    date: '1993-08-13',
+    regEx: 'helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo to you',
+    enum: 'online',
+    firstname: 'Lauryn',
+    lastname: 'Fredi',
+    city: 'Santa Cruz de Tenerife',
+    country: 'Ethiopia',
+    countryCode: 'LI',
+    'email uses current data': 'Lauryn.Fredi@gmail.com',
+    'email from expression': 'Lauryn.Fredi@yopmail.com',
+  };
+
   test('is an object.', async () => {
     assert.that(index).is.ofType('object');
   });
