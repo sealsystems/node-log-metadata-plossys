ab116bb6f8d096780ac03b446a890b76cf7a5b47
balazs4 <balazs4@users.noreply.github.com>
2021-01-05 14:09:09 +0100

    fix: require decorate

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,20 +5,20 @@ const assert = require('assertthat');
 
 const log = require('../lib')(require('@sealsystems/log').getLogger());
 
-suite(`lib/index wraps "require('@sealsystems/log').getLogger()" and the wrapped object`, () => {
-  test('has error function', () => {
+suite(`/lib/index wraps "require('@sealsystems/log').getLogger()" and the wrapped object`, () => {
+  test('has an error function', () => {
     assert.that(log.error).is.ofType('function');
   });
 
-  test('has warn function', () => {
+  test('has a warn function', () => {
     assert.that(log.warn).is.ofType('function');
   });
 
-  test('has info function', () => {
+  test('has an info function', () => {
     assert.that(log.info).is.ofType('function');
   });
 
-  test('has debug function', () => {
+  test('has a debug function', () => {
     assert.that(log.debug).is.ofType('function');
   });
 });
